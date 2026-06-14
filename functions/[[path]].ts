d25052a9a435db83b3d901b15df9de7eef661ba0
Bernard Iorver <53957336+benion28@users.noreply.github.com>
2026-06-14 09:35:40 +0100

    Fixed linting errors

--- functions/[[path]].ts
+++ functions/[[path]].ts
@@ -1,6 +1,7 @@
 // Cloudflare Pages Function - SSR Handler
 // This catches all routes and passes them to TanStack Start's SSR runtime
 
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
 // @ts-ignore - dist/server/server is generated at build time
 import serverEntry from "../dist/server/server";
 
